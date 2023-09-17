28c0755404ed74d9ef0efbe88eb70eac23dc5a45
Yura <Orl0ff.1ur@yandex.ru>
2023-09-17 23:08:00 +0300

    creat h_w 4

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -3,9 +3,11 @@
 -- когда и заказчик и сотрудник зарегистрированы в городе London, а доставку заказа ведет компания United Package (company_name в табл shippers)
 
 SELECT customers.company_name, CONCAT(employees.first_name, ' ', employees.last_name) as emloeer
-FROM customers
-JOIN employees USING(city)
-WHERE customers.city IN ('London') AND employees.city IN ('London')
+FROM orders
+JOIN customers USING(customer_id)
+JOIN employees USING(employee_id)
+JOIN shippers ON orders.ship_via = shippers.shipper_id
+WHERE customers.city IN ('London') AND employees.city IN ('London') AND shippers.company_name IN ('United Package')
 
 -- 2. Наименование продукта, количество товара (product_name и units_in_stock в табл products),
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
